2584d6b825ac0bb9d093776e20392f5958f9b444
Prayas Jadaun <prayashthakur6@gmail.com>
2024-11-01 21:58:55 +0530

    Reset Splash Duration

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -13,7 +13,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Future.delayed(const Duration(seconds: 5), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const MainScreen()),
